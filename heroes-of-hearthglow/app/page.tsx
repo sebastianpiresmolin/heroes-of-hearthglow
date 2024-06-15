1fd616daac3fe2e8452057119059f2451c1cd52b
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-06-15 16:48:54 +0200

    working on navigation

--- heroes-of-hearthglow/app/page.tsx
+++ heroes-of-hearthglow/app/page.tsx
@@ -1,8 +1,10 @@
 import Landing from "./ui/landing"
+import Navigation from "./ui/navigation"
 
 export default function Home() {
   return (
       <div>
+        <Navigation />
         <Landing />
       </div>
   );
---
+++ heroes-of-hearthglow/app/ui/navigation.tsx
@@ -0,0 +1,12 @@
+export default function Navigation() {
+    return(
+        <nav className="fixed top-0 left-0 z-40 navbar bg-dark font-bold text-gray-500">
+            <ul className=" flex gap-3 ">
+                <li className="nav-item">NEWS</li>
+                <li className="nav-item">MEDIA</li>
+                <li className="nav-item">THE TEAM</li>
+                <li className="nav-item">DISCORD</li>
+            </ul>
+        </nav>
+    )
+}
