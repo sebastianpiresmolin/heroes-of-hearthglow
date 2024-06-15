42820cea7554ef159ec74e03e047369699bd6662
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-06-15 08:09:49 +0200

    add responsiveness

--- heroes-of-hearthglow/app/ui/landing.tsx
+++ heroes-of-hearthglow/app/ui/landing.tsx
@@ -11,18 +11,18 @@ export default function Landing() {
                     alt="Landing page"
                     className="absolute top-0 left-0 w-full h-full object-cover z-0"
                 />
-                <div className="absolute mt-[40vh] md:mt-[25vh] top-0 left-0 w-full flex flex-col items-center justify-center z-10">
+                <div className="relative flex-col justify-end w-full flex items-center z-10 h-full mb-20">
                     <Image
                         src="/stardewlogo.webp"
                         width={1000}
                         height={472}
                         alt="Landing page"
-                        className=" max-w-[350px] md:max-w-[500px] xl:max-w-[660px] "
+                        className=" max-w-[350px] md:max-w-[500px] lg:max-w-[660px] "
                     />
                     <a href="https://www.youtube.com" className="text-xl md:text-2xl lg:text-4xl font-bold text-gray-500 mt-10 bg-blue-200 p-4 md:p-6 rounded-3xl">
                         WATCH TRAILER
                     </a>
-                    <h3 className="mt-20 text-lg font-bold text-gray-500">
+                    <h3 className="mb-10 mt-20 text-lg font-bold text-gray-500">
                         ---- Scroll To Explore ----
                     </h3>
                 </div>
