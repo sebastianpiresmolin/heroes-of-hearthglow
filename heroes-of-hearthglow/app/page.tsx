ff43fc5ed9f1baab8c0db5244244fde2892c8812
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-06-17 21:46:00 +0200

    add embedded youtube video

--- heroes-of-hearthglow/app/page.tsx
+++ heroes-of-hearthglow/app/page.tsx
@@ -2,6 +2,7 @@ import Landing from './ui/landing';
 import Navigation from './ui/navigation';
 import Wishlist from './ui/wishlist';
 import Hero from './ui/hero';
+import Youtube from './ui/youtube';
 
 export default function Home() {
   return (
@@ -10,6 +11,8 @@ export default function Home() {
       <Landing />
       <Wishlist />
       <Hero />
+      <Youtube />
+      <Wishlist />
     </div>
   );
 }
