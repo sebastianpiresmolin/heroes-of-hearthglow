5552dbf81dd3c7c35cab3275953feaa93daa1faf
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-06-17 16:26:58 +0200

    built reusable wishlist component

--- heroes-of-hearthglow/app/ui/navigation.tsx
+++ heroes-of-hearthglow/app/ui/navigation.tsx
@@ -69,17 +69,11 @@ export default function Navigation() {
       <NavbarContent justify="end">
         <Link href="#" className="flex-row-reverse">
           <Image
-            className="hidden sm:block"
+            className="w-1/4 m-5 rounded-lg hidden sm:block"
             width={512}
             height={155}
             src="/steam_logo.png"
-            alt="Sign Up"
-            style={{
-              borderRadius: '10px',
-              width: '60%',
-              marginTop: '10px',
-              position: 'relative',
-            }}
+            alt="Steam store link"
           />
         </Link>
       </NavbarContent>
--- heroes-of-hearthglow/app/ui/wishlist.tsx
+++ heroes-of-hearthglow/app/ui/wishlist.tsx
@@ -2,5 +2,34 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 export default function Wishlist() {
-  return <div className="w-full h-[20vh] bg-gray-400"></div>;
+  return (
+    <div className="w-full h-[100px] bg-gray-400 flex justify-center items-center">
+      <div className="flex justify-center items-center max-w-[1050px]">
+        <Image
+          width={244}
+          height={5}
+          src="/wishlist_bar.png"
+          alt="section divider"
+          className="w-[20vw] h-1/3 opacity-30"
+        />
+        <h2 className="font-bold sm:text-xl md:text-2xl  m-5 text-nowrap">
+          Wishlist Now
+        </h2>
+        <Image
+          className="w-1/4 m-5 rounded-lg"
+          width={512}
+          height={155}
+          src="/steam_logo.png"
+          alt="Steam store link"
+        />
+        <Image
+          width={244}
+          height={5}
+          src="/wishlist_bar.png"
+          alt="section divider"
+          className="w-[20vw] h-1/3 opacity-30"
+        />
+      </div>
+    </div>
+  );
 }
