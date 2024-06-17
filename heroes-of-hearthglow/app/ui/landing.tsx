d6e70de77cf8344ee10ce48c0b2e7ce8661cdd8f
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-06-17 14:02:25 +0200

    box shadow to make smooth transition from landing image

--- heroes-of-hearthglow/app/ui/landing.tsx
+++ heroes-of-hearthglow/app/ui/landing.tsx
@@ -3,21 +3,26 @@ import Image from 'next/image';
 export default function Landing() {
   return (
     <section className="relative w-full h-screen">
-      <div className="relative w-full h-full">
+      <div
+        className="relative w-full h-full"
+        style={{ boxShadow: 'inset 0px -25px 5px 0px #9ca3af' }}
+      >
         <Image
           src="/landing.jpg"
           width={2400}
           height={1642}
           alt="Landing page"
-          className="absolute top-0 left-0 w-full h-full object-cover z-0"
+          className="absolute top-0 left-0 w-full h-full object-cover"
+          style={{ zIndex: -1 }}
         />
         <div className="relative flex-col justify-end w-full flex items-center z-10 h-full mb-20">
           <Image
             src="/stardewlogo.webp"
             width={1000}
             height={472}
-            alt="Landing page"
-            className=" max-w-[350px] md:max-w-[500px] lg:max-w-[660px] "
+            alt="Landing page logo"
+            className="max-w-[350px] md:max-w-[500px] lg:max-w-[660px]"
+            style={{ zIndex: 0 }}
           />
           <a
             href="https://www.youtube.com"
---
+++ heroes-of-hearthglow/app/ui/wishlist.tsx
@@ -0,0 +1,6 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function Wishlist() {
+  return <div className="w-full h-[20vh] bg-gray-400"></div>;
+}
