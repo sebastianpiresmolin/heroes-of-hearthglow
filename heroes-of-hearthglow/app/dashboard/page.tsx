d883a9a4ccea6ba099641ad67624502574c358a7
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-06-21 22:42:33 +0200

    moved logout function to seperate ui component and cleaned up redundant code. This component is placeholder until dahboard navigation is in place

--- heroes-of-hearthglow/app/dashboard/page.tsx
+++ heroes-of-hearthglow/app/dashboard/page.tsx
@@ -1,29 +1,9 @@
-'use client';
+import Logout from '@/app/ui/logout';
 
-import { useState, useEffect } from 'react';
-import { usePathname, useRouter } from 'next/navigation';
-import clsx from 'clsx';
-import axios from 'axios';
-import { useAuth } from '../contexts/authContext';
-
-export default function Dashboard() {
-  const router = useRouter();
-  const { isLoggedIn, setLoggedIn } = useAuth();
-
-  const logout = async (event: any) => {
-    event.preventDefault();
-    try {
-      await axios.get('/api/auth/logout');
-      setLoggedIn(false);
-      router.push('/login');
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
+export default function dashboard() {
   return (
     <div>
-      <a onClick={logout}>logga ut</a>
+      <Logout />
     </div>
   );
 }
