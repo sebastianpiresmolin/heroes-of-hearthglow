2e3de443d197a6ece01fda85b0df1ab5a1ae8dad
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-06-29 18:27:58 +0200

    made the getfivelatestnews into an api to fetch isntead because i need the use client.

--- heroes-of-hearthglow/__tests__/data.spec.ts
+++ heroes-of-hearthglow/__tests__/data.spec.ts
@@ -1,5 +1,4 @@
 import { getLatestNews } from '../app/lib/data';
-import { getLatestFiveNews } from '../app/lib/data';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { News } from '../app/lib/data';
@@ -61,73 +60,5 @@ describe('getLatestNews', () => {
     const latestNews = await getLatestNews();
     expect(latestNews).toHaveLength(1);
     expect(latestNews[0].title).toBe('Latest News');
-  });
-});
-
-//getLatestFiveNews
-
-describe('getLatestFiveNews', () => {
-  let mongoServer: MongoMemoryServer;
-
-  beforeAll(async () => {
-    mongoServer = await MongoMemoryServer.create();
-    const uri = mongoServer.getUri();
-    console.log(`Connecting to URI: ${uri}`);
-    await mongoose.connect(uri, { dbName: 'verifyMASTER' });
-  });
-
-  afterAll(async () => {
-    await mongoose.disconnect();
-    await mongoServer.stop();
-  });
-
-  it('should return the latest news', async () => {
-    await News.create({
-      id: 1,
-      title: 'First News',
-      time: new Date(),
-      image: 'https://example.com/first-news.jpg',
-      description: 'This is the first news item',
-    });
-    await News.create({
-      id: 2,
-      title: 'Not Latest News',
-      time: new Date(),
-      image: 'https://example.com/latest-news.jpg',
-      description: 'This is not the latest news item',
-    });
-    await News.create({
-      id: 3,
-      title: 'Not Latest News',
-      time: new Date(),
-      image: 'https://example.com/latest-news.jpg',
-      description: 'This is not the latest news item',
-    });
-    await News.create({
-      id: 4,
-      title: 'Not Latest News',
-      time: new Date(),
-      image: 'https://example.com/latest-news.jpg',
-      description: 'This is not the latest news item',
-    });
-    await News.create({
-      id: 5,
-      title: 'Not Latest News',
-      time: new Date(),
-      image: 'https://example.com/latest-news.jpg',
-      description: 'This is not the latest news item',
-    });
-    await News.create({
-      id: 6,
-      title: 'Latest News',
-      time: new Date(),
-      image: 'https://example.com/latest-news.jpg',
-      description: 'This is the latest news item',
-    });
-
-    const latestFiveNews = await getLatestFiveNews();
-    expect(latestFiveNews).toHaveLength(5);
-    expect(latestFiveNews[0].title).toBe('Latest News');
-    expect(latestFiveNews[4].id).toBe(2);
   });
 });
--- heroes-of-hearthglow/app/dashboard/page.tsx
+++ heroes-of-hearthglow/app/dashboard/page.tsx
@@ -1,8 +1,28 @@
-import LatestFiveNews from '../ui/latest-five-news';
+'use client';
+import { Divider } from '@nextui-org/react';
+import { useEffect, useState } from 'react';
 
 export default function DashboardHome() {
+  interface NewsItem {
+    id: number;
+    title: string;
+    time: string;
+  }
+
+  const [news, setNews] = useState<NewsItem[]>([]);
+
+  useEffect(() => {
+    async function fetchNews() {
+      const response = await fetch('/api/news/fivelatestnews');
+      const data = await response.json();
+      setNews(data);
+    }
+
+    fetchNews();
+  }, []);
+
   return (
-    <div>
+    <div className="flex">
       <div className="flex-col w-3/5 h-full">
         <div className="flex gap-10">
           <div className="bg-neutral-900 leading-10 w-full p-10 shadow-sm shadow-black outline outline-1 outline-zinc-700 rounded-lg">
@@ -21,7 +41,31 @@ export default function DashboardHome() {
           </div>
         </div>
         <div>
-          <LatestFiveNews />
+          <div className="bg-neutral-900 flec-col p-10 mt-10 shadow-sm shadow-black outline outline-1 outline-zinc-700 rounded-lg">
+            <h1 className="text-trueGray-50 text-4xl font-bold">Latest News</h1>
+            <p className="text-zinc-400">The latest five news updates</p>
+            <div className="flex justify-between mt-10 mr-2 ml-2">
+              <p className="text-zinc-400">ID</p>
+              <p className="text-zinc-400 basis-3/4">Title</p>
+              <p className="text-zinc-400">Date</p>
+            </div>
+            {news.map((news) => (
+              <div
+                key={news.id}
+                className="flex-col justify-between mt-8 text-2xl"
+              >
+                <div className="flex justify-between align-middle">
+                  <p className="text-trueGray-50  ">
+                    <span className="text-zinc-400"># </span>
+                    {news.id}
+                  </p>
+                  <p className="text-trueGray-50 basis-3/4">{news.title}</p>
+                  <p className="text-trueGray-50">{news.time}</p>
+                </div>
+                <Divider className="bg-zinc-600 mt-8" />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
       <div></div>
