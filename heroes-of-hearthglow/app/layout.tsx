16dffc0c0aba31f7b6b496f92ccab181c7dc7c58
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-07-17 20:14:46 +0200

    add google analytics script tag

--- heroes-of-hearthglow/app/layout.tsx
+++ heroes-of-hearthglow/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import { roboto } from '@/app/ui/fonts';
 import { AuthProvider } from '@/app/contexts/authContext';
+import Head from 'next/head';
 
 export default function RootLayout({
   children,
@@ -10,6 +11,22 @@ export default function RootLayout({
   return (
     <AuthProvider>
       <html lang="en">
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-2BVGJLB2SC"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-2BVGJLB2SC');
+              `,
+            }}
+          />
+        </Head>
         <body className={`${roboto.className} antialiased`}>{children}</body>
       </html>
     </AuthProvider>
