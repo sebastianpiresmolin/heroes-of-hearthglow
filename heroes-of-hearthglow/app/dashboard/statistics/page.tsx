20bea8464ef3ed12b8231539e6bbc61b21df4c23
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-07-21 22:19:37 +0200

    trying to identify issue with fetch

--- heroes-of-hearthglow/app/dashboard/statistics/page.tsx
+++ heroes-of-hearthglow/app/dashboard/statistics/page.tsx
@@ -13,11 +13,17 @@ type GA4AnalyticsData = {
 };
 
 async function fetchAnalyticsData(url: string): Promise<GA4AnalyticsData> {
-  const res = await fetch(url);
-  if (!res.ok) {
-    throw new Error('Failed to fetch data');
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      console.error(`Failed to fetch data from ${url}. Status: ${res.status}`);
+      throw new Error('Failed to fetch data');
+    }
+    return res.json();
+  } catch (error) {
+    console.error(`Error fetching data from ${url}:`, error);
+    throw error; // Rethrow to ensure the error is not silently caught
   }
-  return res.json();
 }
 
 export default async function AnalyticsPage() {
@@ -57,6 +63,42 @@ export default async function AnalyticsPage() {
   const oneYearDataCompare = await fetchAnalyticsData(
     'https://heroes-of-hearthglow.vercel.app/api/analytics/oneYearUsersCompare'
   );
+  /*const oneDayData = await fetchAnalyticsData(
+    'http://localhost:3000/api/analytics/oneDaysUsers'
+  );
+  const oneDayDataCompare = await fetchAnalyticsData(
+    'http://localhost:3000/api/analytics/oneDaysUsersCompare'
+  );
+  const oneWeekData = await fetchAnalyticsData(
+    'http://localhost:3000/api/analytics/oneWeekUsers'
+  );
+  const oneWeekDataCompare = await fetchAnalyticsData(
+    'http://localhost:3000/api/analytics/oneWeekUsersCompare'
+  );
+  const oneMonthData = await fetchAnalyticsData(
+    'http://localhost:3000/api/analytics/oneMonthUsers'
+  );
+  const oneMonthDataCompare = await fetchAnalyticsData(
+    'http://localhost:3000/api/analytics/oneMonthUsersCompare'
+  );
+  const threeMonthData = await fetchAnalyticsData(
+    'http://localhost:3000/api/analytics/threeMonthUsers'
+  );
+  const threeMonthDataCompare = await fetchAnalyticsData(
+    'http://localhost:3000/api/analytics/threeMonthUsersCompare'
+  );
+  const sixMonthData = await fetchAnalyticsData(
+    'http://localhost:3000/api/analytics/sixMonthUsers'
+  );
+  const sixMonthDataCompare = await fetchAnalyticsData(
+    'http://localhost:3000/api/analytics/sixMonthUsersCompare'
+  );
+  const oneYearData = await fetchAnalyticsData(
+    'http://localhost:3000/api/analytics/oneYearUsers'
+  );
+  const oneYearDataCompare = await fetchAnalyticsData(
+    'http://localhost:3000/api/analytics/oneYearUsersCompare'
+  );*/
 
   return (
     <div className="text-trueGray-50 flex-col">
