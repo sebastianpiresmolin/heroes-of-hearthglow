8254ba719ee2122de42c26db7072a1ea222c1869
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-11-06 16:38:24 +0100

    added transitions to hero section. shifted around the image with z-index and gave it a gradient

--- heroes-of-hearthglow/app/ui/hero.tsx
+++ heroes-of-hearthglow/app/ui/hero.tsx
@@ -1,27 +1,77 @@
-import Image from 'next/image';
+'use client'
 
-export default function Hero() {
+import Image from 'next/image';
+import React, { useEffect, useRef } from 'react';
+
+const Hero = () => {
+  const h2Ref = useRef<HTMLHeadingElement>(null);
+  const pRef = useRef<HTMLParagraphElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+        (entries, observer) => {
+          entries.forEach(entry => {
+            if (entry.isIntersecting) {
+              entry.target.classList.add('slide-in');
+              observer.unobserve(entry.target);
+            }
+          });
+        },
+        {
+          threshold: 0.1,
+        }
+    );
+
+    if (h2Ref.current) {
+      observer.observe(h2Ref.current);
+    }
+    if (pRef.current) {
+      observer.observe(pRef.current);
+    }
+
+    // Clean up
+    return () => {
+      if (h2Ref.current) {
+        observer.unobserve(h2Ref.current);
+      }
+      if (pRef.current) {
+        observer.unobserve(pRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <section>
-      <div className="flex justify-center bg-black outer-shadow ">
-        <div
-          className=" max-w-[1050px] flex-col h-fit bg-cover bg-center justify-center w-[90vw] inset-shadow"
-          style={{
-            backgroundImage: "url('/landingGray.png')",
-          }}
-        >
-          <h2 className="mb-10 text-4xl font-bold text-white drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl text-center pt-20">
+      <section className="relative bg-black flex flex-col items-center h-screen outer-shadow">
+        <div className="relative w-full max-w-[800px]">
+          <Image
+              src="/landingGray.png"
+              width={2400}
+              height={1642}
+              alt="Landing page"
+              className="w-full h-full object-cover opacity-50 pt-10"
+          />
+          <div className="absolute inset-0 bg-black bg-opacity-50">
+            <div className="absolute inset-0 bg-radial from-black to-transparent"></div>
+          </div>
+        </div>
+        <div className="absolute flex flex-col justify-center z-10">
+          <h2
+              ref={h2Ref}
+              className="text-center mb-10 text-3xl font-bold text-gray-100 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl pt-20 hidden-slide-in"
+          >
             Welcome to Hearthglow
           </h2>
-          <p className="max-w-[500px] pb-20 text-xl w-[80vw] m-auto font-semibold text-white drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl text-justify">
+          <p
+              ref={pRef}
+              className="max-w-[500px] pb-20 text-center lg:text-justify text-xl w-[80vw] m-auto mt-5 font-semibold text-gray-100 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl hidden-slide-in"
+          >
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-            sed arcu nec velit congue ultrices non nec massa. Donec eu cursus
-            leo. Phasellus ornare consequat euismod. Curabitur sed arcu nec
-            velit congue ultrices non nec massa. Donec eu cursus leo. Phasellus
-            ornare consequat euismod.
+            sed arcu nec velit congue ultrices non nec massa. Donec eu cursus leo.
+            Phasellus ornare consequat euismod.
           </p>
         </div>
-      </div>
-    </section>
+      </section>
   );
-}
+};
+
+export default Hero;
