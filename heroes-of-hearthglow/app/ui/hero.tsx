a202f5561a771409146ed4dab186fed00ca14966
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-11-15 21:15:55 +0100

    Refactor hero component layout and text styles
    Updated flexbox utilities for better alignment and spacing in different screen sizes. Increased margin for better readability and consistency. Enhanced text content to improve visual appeal and emphasis on the message.

--- heroes-of-hearthglow/app/ui/hero.tsx
+++ heroes-of-hearthglow/app/ui/hero.tsx
@@ -78,7 +78,31 @@ const Hero = () => {
           </p>
         </div>
         <div className="flex-col max-w-[1000px] w-full h-fit z-1">
-          <div className="flex flex-col justify-center items-left mt-[600px] md:flex-row">
+          <div className="flex flex-col justify-start items-start mt-[600px] md:flex-row md:mt-0 md:items-center">
+            <Image
+              src="/landing.jpg"
+              width={2400}
+              height={1642}
+                alt="Landing page"
+                className="w-1/2 ml-5 border-1 border-amber-400 rounded-md border-opacity-50 "
+            />
+            <p className="ml-5 mt-2 w-7/8 md:leading-9 font-normal text-slate-300 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl md: mr-5">
+              Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably.
+            </p>
+          </div>
+          <div className="flex flex-col justify-end items-end mt-8 md:flex-row-reverse md:items-center md:mt-16">
+            <Image
+                src="/landing.jpg"
+                width={2400}
+                height={1642}
+                alt="Landing page"
+                className="w-1/2 mr-5 border-1 border-amber-400 rounded-md border-opacity-50"
+            />
+            <p className="ml-5 mt-2 w-7/8 md:leading-9 font-normal md:text-left text-right text-slate-300 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl md: mr-5">
+              Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably.
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-left mt-8 md:flex-row md:items-center md:mt-16 ">
             <Image
                 src="/landing.jpg"
                 width={2400}
@@ -86,11 +110,11 @@ const Hero = () => {
                 alt="Landing page"
                 className="w-1/2 ml-5 border-1 border-amber-400 rounded-md border-opacity-50 "
             />
-            <p className="ml-5 mt-2 text-center w-1/2 font-normal text-gray-100 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl md: mr-5">
-              Fight bad dudes at badass places, like the place shown above probably.
+            <p className="ml-5 mt-2 w-7/8 md:leading-9 font-normal text-slate-300 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl md: mr-5">
+              Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably.
             </p>
           </div>
-          <div className="flex flex-col justify-end items-end mt-4">
+          <div className="flex flex-col justify-end items-end mt-8 md:flex-row-reverse md:items-center md:mt-16">
             <Image
                 src="/landing.jpg"
                 width={2400}
@@ -98,32 +122,8 @@ const Hero = () => {
                 alt="Landing page"
                 className="w-1/2 mr-5 border-1 border-amber-400 rounded-md border-opacity-50 "
             />
-            <p className="ml-5 mt-2 text-center w-1/2 font-normal text-gray-100 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl md: mr-5">
-              Fight bad dudes at badass places, like the place shown above probably.
-            </p>
-          </div>
-          <div className="flex flex-col justify-center items-left mt-4">
-            <Image
-                src="/landing.jpg"
-                width={2400}
-                height={1642}
-                alt="Landing page"
-                className="w-1/2 ml-5 border-1 border-amber-400 rounded-md border-opacity-50 "
-            />
-            <p className="ml-5 mt-2 text-center w-1/2 font-normal text-gray-100 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl md: mr-5">
-              Fight bad dudes at badass places, like the place shown above probably.
-            </p>
-          </div>
-          <div className="flex flex-col justify-end items-end mt-4">
-            <Image
-                src="/landing.jpg"
-                width={2400}
-                height={1642}
-                alt="Landing page"
-                className="w-1/2 mr-5 border-1 border-amber-400 rounded-md border-opacity-50 "
-            />
-            <p className="ml-5 mt-2 text-center w-1/2 font-normal text-gray-100 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl md: mr-5">
-              Fight bad dudes at badass places, like the place shown above probably.
+            <p className="ml-5 mt-2 w-7/8 md:leading-9 font-normal md:text-left text-right text-slate-300 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl md: mr-5">
+              Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably.
             </p>
           </div>
         </div>
