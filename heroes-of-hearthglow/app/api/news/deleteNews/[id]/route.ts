f63d53dd2c90d7b6e39a1b5c70ad5ecae43cc8dd
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-11-26 21:54:04 +0100

    Add Swagger documentation for news API routes
    Included detailed Swagger annotations for routes: /fivelatestnews, /deleteNews/[id], /latestId, /getById, and /editNewsItem. These annotations describe endpoints, parameters, and response schemas to improve API documentation and usability.

--- heroes-of-hearthglow/app/api/news/deleteNews/[id]/route.ts
+++ heroes-of-hearthglow/app/api/news/deleteNews/[id]/route.ts
@@ -4,6 +4,72 @@ import { News } from '../../../../lib/data';
 
 connect();
 
+/**
+ * @swagger
+ * /api/news/deleteNews/{id}:
+ *  delete:
+ *    description: Delete a News item by ID
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        schema:
+ *          type: string
+ *        description: The ID of the news item to delete
+ *    responses:
+ *      200:
+ *        description: Successful response
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: string
+ *              example: 'News item deleted successfully'
+ *      400:
+ *        description: Bad Request
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *              example:
+ *                message: 'ID path parameter is missing'
+ *      406:
+ *        description: Invalid Format
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *              example:
+ *                message: 'Invalid ID format'
+ *      404:
+ *        description: Not Found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *              example:
+ *                message: 'News item not found or already deleted'
+ *      500:
+ *        description: Internal Server Error
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *              example:
+ *                message: 'Internal Server Error'
+ */
+
 export async function DELETE(
   request: NextRequest,
   { params }: { params: { id: string } }
@@ -18,7 +84,7 @@ export async function DELETE(
     const numericId = Number(id);
 
     if (isNaN(numericId)) {
-      return new Response('Invalid ID format', { status: 400 });
+      return new Response('Invalid ID format', { status: 406 });
     }
 
     const deleteResult = await News.deleteOne({ id: numericId });
--- heroes-of-hearthglow/app/api/news/fivelatestnews/route.ts
+++ heroes-of-hearthglow/app/api/news/fivelatestnews/route.ts
@@ -4,6 +4,49 @@ import { News } from '../../../lib/data';
 
 connect();
 
+/**
+ * @swagger
+ * /api/news/fivelatestnews:
+ *  get:
+ *    description: Retrieve the latest 5 news items
+ *    responses:
+ *      200:
+ *        description: Successful response
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                type: object
+ *                properties:
+ *                  _id:
+ *                    type: string
+ *                    example: "67387d4be3e3bf49866874f0"
+ *                  title:
+ *                    type: string
+ *                    example: "example"
+ *                  description:
+ *                    type: string
+ *                    example: "example"
+ *                  image:
+ *                    type: string
+ *                    example: "image url"
+ *                  id:
+ *                    type: integer
+ *                    example: 0
+ *                  time:
+ *                    type: string
+ *                    format: date-time
+ *                    example: "2024-11-26T00:00:00Z"
+ *      500:
+ *        description: Internal Server Error
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: string
+ *              example: 'Internal Server Error'
+ */
+
 export async function GET(request: NextRequest) {
   try {
     const news = await News.find().sort({ id: -1 }).limit(5); // Fetch 5 latest news items
