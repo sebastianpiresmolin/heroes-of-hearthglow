00cec4a4b1dc231c83405836bf6611fc291ca090
sebastianpiresmolin <piresmolinsecure@gmail.com>
2025-01-12 11:31:13 +0100

    Fix database connection handling and improve error response
    Moved database connection initialization into the GET request handler to ensure it's properly established. Adjusted radix for page number parsing to decimal and updated error handling to provide more descriptive responses. Minor comment and format improvements for clarity.

--- heroes-of-hearthglow/app/api/news/allnews/route.ts
+++ heroes-of-hearthglow/app/api/news/allnews/route.ts
@@ -2,28 +2,35 @@ import { NextRequest, NextResponse } from 'next/server';
 import { connect } from '../../../lib/dbconnect';
 import { News } from '../../../lib/data';
 
-connect();
+
 
 export async function GET(request: NextRequest) {
   try {
-    const url = new URL(request.url);
-    const page = parseInt(url.searchParams.get('page') || '1', 8); // Parse page number from query string, 8 is the radix
+    await connect(); // Säkerställ att databasen ansluter
 
+    const url = new URL(request.url);
+
+    // Parse page number från query string
+    const page = parseInt(url.searchParams.get('page') || '1', 10);
     const itemsPerPage = 8;
     const skip = (page - 1) * itemsPerPage;
 
-    // Fetch the total count of news items
+    // Fetch total count av nyheter
     const totalCount = await News.countDocuments();
 
-    // Fetch the news items with pagination
+    // Fetch news items med paginering
     const news = await News.find()
-      .sort({ id: -1 })
-      .limit(itemsPerPage)
-      .skip(skip);
+        .sort({ id: -1 })
+        .limit(itemsPerPage)
+        .skip(skip);
 
-    // Return news items and total count
+    // Returnera data
     return NextResponse.json({ news, totalCount });
   } catch (error: any) {
-    return NextResponse.error();
+    console.error('Error in GET /api/news/allnews:', error);
+    return NextResponse.json(
+        { error: 'Error retrieving news data' },
+        { status: 500 }
+    );
   }
 }
