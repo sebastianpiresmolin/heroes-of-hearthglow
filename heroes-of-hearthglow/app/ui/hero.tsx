11bb0cdd8412601dc00590520603b5fea9a11aa4
sebastianpiresmolin <piresmolinsecure@gmail.com>
2025-01-12 21:08:00 +0100

    Update hero section images and adjust layout styles
    Replaced image assets in the hero section with updated visuals and refined layout styling for improved responsiveness and alignment. Changes include updated image sources, adjusted element spacing, and justified text alignment for better readability.

--- heroes-of-hearthglow/app/ui/hero.tsx
+++ heroes-of-hearthglow/app/ui/hero.tsx
@@ -44,7 +44,7 @@ const Hero = () => {
       <section className="relative bg-black flex flex-col items-center h-fit outer-shadow">
         <div className=" flex-col relative w-full max-w-[800px]">
           <Image
-              src="/landingGray.png"
+              src="/Logo.png"
               width={2400}
               height={1642}
               alt="Landing page"
@@ -54,6 +54,7 @@ const Hero = () => {
             <div className="absolute inset-0 bg-radial from-black to-transparent"></div>
           </div>
         </div>
+
         <div className="absolute flex flex-col justify-center items-center z-1">
           <h2
               ref={h2Ref}
@@ -75,52 +76,40 @@ const Hero = () => {
             within.
           </p>
         </div>
-        <div className="flex-col max-w-[1000px] w-full h-fit z-1">
-          <div className="flex flex-col justify-start items-start mt-[600px] md:flex-row md:mt-0 md:items-center">
+        <div className="flex-col max-w-[1000px] w-full h-fit z-1 md:mt-40">
+          <div className="flex flex-col justify-start items-center mt-[600px] md:flex-row md:mt-0 md:items-start">
             <Image
-              src="/landing.jpg"
+              src="/BlacksmithScene.png"
               width={2400}
               height={1642}
                 alt="Landing page"
-                className="w-1/2 ml-5 border-1 border-amber-400 rounded-md border-opacity-50 "
+                className="w-2/3 mb-4 md:mb-0 md:w-1/3 md:ml-5 md:mr-5  border-1 md:border-2 border-amber-400 rounded-md border-opacity-50 "
             />
-            <p className="ml-5 mt-2 w-7/8 md:leading-9 font-normal text-slate-300 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl md: mr-5">
+            <p className="ml-5 mt-2 w-7/8 text-justify md:leading-9 font-normal text-slate-300 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl md: mr-5">
               Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably.
             </p>
           </div>
-          <div className="flex flex-col justify-end items-end mt-8 md:flex-row-reverse md:items-center md:mt-16">
+          <div className="flex flex-col justify-end items-center mt-16 md:flex-row-reverse md:items-start md:mt-16">
             <Image
-                src="/landing.jpg"
+                src="/BlacksmithScene.png"
                 width={2400}
                 height={1642}
                 alt="Landing page"
-                className="w-1/2 mr-5 border-1 border-amber-400 rounded-md border-opacity-50"
+                className="w-2/3 mb-4 md:mb-0 md:w-1/3 md:mr-5 md:ml-5 border-1 md:border-2 border-amber-400 rounded-md border-opacity-50 "
             />
-            <p className="ml-5 mt-2 w-7/8 md:leading-9 font-normal md:text-left text-right text-slate-300 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl md: mr-5">
+            <p className="ml-5 mt-2 w-7/8 text-justify md:leading-9 font-normal text-slate-300 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl md: mr-5">
               Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably.
             </p>
           </div>
-          <div className="flex flex-col justify-center items-left mt-8 md:flex-row md:items-center md:mt-16 ">
+          <div className="flex flex-col justify-center items-center mt-16 md:flex-row md:items-start md:mt-16 ">
             <Image
-                src="/landing.jpg"
+                src="/BlacksmithScene.png"
                 width={2400}
                 height={1642}
                 alt="Landing page"
-                className="w-1/2 ml-5 border-1 border-amber-400 rounded-md border-opacity-50 "
+                className="w-2/3 mb-4 md:mb-0 md:w-1/3 md:ml-5 md:mr-5 border-1 md:border-2 border-amber-400 rounded-md border-opacity-50 "
             />
-            <p className="ml-5 mt-2 w-7/8 md:leading-9 font-normal text-slate-300 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl md: mr-5">
-              Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably.
-            </p>
-          </div>
-          <div className="flex flex-col justify-end items-end mt-8 md:flex-row-reverse md:items-center md:mt-16">
-            <Image
-                src="/landing.jpg"
-                width={2400}
-                height={1642}
-                alt="Landing page"
-                className="w-1/2 mr-5 border-1 border-amber-400 rounded-md border-opacity-50 "
-            />
-            <p className="ml-5 mt-2 w-7/8 md:leading-9 font-normal md:text-left text-right text-slate-300 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl md: mr-5">
+            <p className="ml-5 mt-2 w-7/8 text-justify md:leading-9 font-normal text-slate-300 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl md: mr-5">
               Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably. Fight bad dudes at badass places, like the place shown above probably.
             </p>
           </div>
