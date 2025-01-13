3698496bb647d359076b2a3e170bf1f621ec1630
sebastianpiresmolin <piresmolinsecure@gmail.com>
2025-01-13 21:50:16 +0100

    Add unique IDs to navbar elements for better accessibility
    Unique IDs were added to various navbar elements to ensure better accessibility, improve DOM element targeting, and assist in testing/debugging. This change also enhances maintainability by providing clear references to key UI components.

--- heroes-of-hearthglow/app/ui/navigation.tsx
+++ heroes-of-hearthglow/app/ui/navigation.tsx
@@ -18,12 +18,14 @@ export default function Navigation() {
 
   return (
     <Navbar
+        id="navbar"
       key={key}
       onMenuOpenChange={setIsMenuOpen}
       className="fixed w-[100vw] bg-black bg-opacity-60 no-bg-effect custom-navbar"
     >
       <NavbarContent className="">
         <NavbarMenuToggle
+            id="navbar-menu-toggle-mobile"
           aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
           className="sm:hidden text-amber-400"
         />
@@ -33,6 +35,7 @@ export default function Navigation() {
               className="hidden sm:block">
             <div className="flex items-center justify-center w-[200px] h-[63px] m-auto rounded-md">
               <Image
+                  id="home-link-navbar"
                   width={200}
                   height={63}
                   alt="link to steam store"
@@ -46,6 +49,7 @@ export default function Navigation() {
         <NavbarContent justify="end">
           <Link href="#" >
             <Image
+                id="discord-link-navbar"
               className="w-[50px] rounded-lg hidden sm:block hover:scale-95"
               width={512}
               height={155}
@@ -55,6 +59,7 @@ export default function Navigation() {
           </Link>
           <Link href="#">
             <Image
+                id="youtube-link-navbar"
                 className="w-[80px] rounded-lg hidden sm:block hover:scale-95"
                 width={512}
                 height={155}
@@ -64,6 +69,7 @@ export default function Navigation() {
           </Link>
           <Link href="#">
             <Image
+                id="steam-link-navbar"
                 className="w-[128px] rounded-lg hidden sm:block hover:scale-95"
                 width={512}
                 height={155}
@@ -73,12 +79,14 @@ export default function Navigation() {
           </Link>
         </NavbarContent>
       <NavbarMenu
+          id="navbar-menu-mobile"
           className="custom-navbar bg-black bg-opacity-80 flex items-center outer-shadow ">
         <Link
             href="/"
         >
           <div className="flex items-center justify-center w-[200px] h-[63px] m-auto rounded-md">
             <Image
+                id="home-link-navbar-mobile"
                 width={200}
                 height={63}
                 alt="link to steam store"
@@ -90,6 +98,7 @@ export default function Navigation() {
         <Link
             className="amber-shadow mt-12">
           <Image
+              id="steam-link-navbar-mobile"
               width={200}
               height={63}
               alt="link to steam store"
@@ -100,6 +109,7 @@ export default function Navigation() {
         <Link
             className="amber-shadow mt-3 w-[200px] h-[63px] rounded-md border-2 border-black items-center justify-center bg-blue-700 ">
           <Image
+              id="discord-link-navbar-mobile"
               width={200}
               height={63}
               alt="link to steam store"
@@ -110,6 +120,7 @@ export default function Navigation() {
         <Link
             className="amber-shadow mt-3 w-[200px] h-[63px] rounded-md border-2 border-black items-center justify-center bg-trueGray-300 ">
           <Image
+              id="youtube-link-navbar-mobile"
               width={200}
               height={100}
               alt="link to steam store"
@@ -119,19 +130,23 @@ export default function Navigation() {
         </Link>
         <div className="absolute bottom-0 flex-col justify-center w-full h-fit pt-20 pb-14">
           <Image
+              id="studio-logo-navbar-mobile"
               alt="studio logo"
               height={80}
               className="rounded-full m-auto"
               src="/studio-logo.png"
               width={80}
           />
-          <h2 className="text-2xl font-bold text-slate-400 opacity-80 text-center p-5">
+          <h2 id="studio-name-navbar-mobile"
+              className="text-2xl font-bold text-slate-400 opacity-80 text-center p-5">
             Ramen Cat Studios
           </h2>
-          <p className="text-center text-slate-400 opacity-80">
+          <p id="studio-copyright-navbar-mobile"
+              className="text-center text-slate-400 opacity-80">
             © 2024 Ramen Cat Studios.<br/>All rights reserved.<br/>ramen@cat.email
           </p>
-          <p className="text-center text-slate-400 opacity-80 mt-2">
+          <p id="website-author-navbar-mobile"
+              className="text-center text-slate-400 opacity-80 mt-2">
             Website by <a href="https://github.com/sebastianpiresmolin" className="text-cyan-50 hover:text-cyan-400">Sebastian
             Molin</a>
           </p>
