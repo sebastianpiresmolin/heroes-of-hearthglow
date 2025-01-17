f99977581550b14ace1b8d7783b52ae8ee134065
sebastianpiresmolin <piresmolinsecure@gmail.com>
2025-01-17 17:47:27 +0100

    Enhance UI with updated text styling and layout adjustments

--- heroes-of-hearthglow/app/ui/hero.tsx
+++ heroes-of-hearthglow/app/ui/hero.tsx
@@ -58,13 +58,13 @@ const Hero = () => {
         <div className="absolute flex flex-col justify-center items-center z-1">
           <h2
               ref={h2Ref}
-              className="text-center mb-10 mr-5 ml-5 text-4xl font-semibold text-slate-100 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl pt-20 hidden-slide-in"
+              className="text-center mb-10 mr-5 ml-5 text-4xl text-amber-400 font-semibold drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl pt-20 hidden-slide-in"
           >
             Let Your Spirit Burn Brighter Than Any Evil
           </h2>
           <p
               ref={pRef}
-              className="max-w-[500px] text-justify lg:text-justify md:text-md w-[80vw] leading-9 tracking-wide font-normal text-slate-300 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl hidden-slide-in"
+              className="max-w-[500px] text-amber-50 lg:text-xl text-justify lg:text-justify md:text-md w-[80vw] leading-9 tracking-wide font-normal text-slate-300 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl hidden-slide-in"
           >
             Heroes of Hearthglow is a 2D action RPG roguelike where your fate is forged by fire and tempered through
             endless rebirth. Each time you fall, you rise again—stronger, more determined, and ever closer to banishing
@@ -77,37 +77,37 @@ const Hero = () => {
           </p>
         </div>
         <div className="flex-col max-w-[1000px] w-full h-fit z-1 md:mt-40">
-          <div className="flex flex-col justify-start items-center mt-[600px] md:flex-row md:mt-0 md:items-start">
+          <div className="flex flex-col justify-start items-center md:flex-row lg:items-center lg:justify-items-center mt-[600px] md:mt-0">
             <Image
               src="/gameplay.png"
               width={2400}
               height={1642}
                 alt="Landing page"
-                className="w-2/3 mb-4 md:mb-0 md:w-1/3 md:ml-5 md:mr-5  border-1 md:border-2 border-amber-400 rounded-md border-opacity-50 "
+                className="w-2/3 mb-4 md:mb-0 md:w-2/5 md:ml-5 md:mr-5 border-1 md:border-2 border-amber-400 rounded-md border-opacity-50 "
             />
-            <p className="ml-5 mt-2 w-7/8 text-justify md:leading-9 font-normal text-slate-300 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl md: mr-5">
+            <p className="ml-5 mt-2 w-7/8 text-amber-50 lg:text-3xl text-justify md:leading-9 font-normal text-slate-300 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl md: mr-5">
               Dive into the action-packed adventure! Battle all manner of creatures while combining hundreds of weapons, armors, and spells to craft your preferred playstyle.            </p>
           </div>
-          <div className="flex flex-col justify-end items-center mt-16 md:flex-row-reverse md:items-start md:mt-16">
+          <div className="flex flex-col justify-start items-center md:flex-row-reverse lg:items-center lg:justify-items-center mt-8 md:mt-0">
             <Image
                 src="/SwampScene.png"
                 width={2400}
                 height={1642}
                 alt="Landing page"
-                className="w-2/3 mb-4 md:mb-0 md:w-1/3 md:mr-5 md:ml-5 border-1 md:border-2 border-amber-400 rounded-md border-opacity-50 "
+                className="w-2/3 mb-4 md:mb-0 md:w-2/5 md:mr-5 md:ml-5 border-1 md:border-2 border-amber-400 rounded-md border-opacity-50 "
             />
-            <p className="ml-5 mt-2 w-7/8 text-justify md:leading-9 font-normal text-slate-300 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl md: mr-5">
+            <p className="ml-5 mt-2 w-7/8 text-amber-50 lg:text-3xl text-justify md:leading-9 font-normal text-slate-300 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl md: mr-5">
               Meet Croakus and Poakus, the mystical frog wizards ready to assist with their arcane powers and alchemical expertise.            </p>
           </div>
-          <div className="flex flex-col justify-center items-center mt-16 md:flex-row md:items-start md:mt-16 ">
+          <div className="flex flex-col justify-start items-center md:flex-row lg:items-center lg:justify-items-center mt-8 md:mt-0">
             <Image
                 src="/BlacksmithScene.png"
                 width={2400}
                 height={1642}
                 alt="Landing page"
-                className="w-2/3 mb-4 md:mb-0 md:w-1/3 md:ml-5 md:mr-5 border-1 md:border-2 border-amber-400 rounded-md border-opacity-50 "
+                className="w-2/3 mb-4 md:mb-0 md:w-2/5 md:ml-5 md:mr-5 border-1 md:border-2 border-amber-400 rounded-md border-opacity-50 "
             />
-            <p className="ml-5 mt-2 w-7/8 text-justify md:leading-9 font-normal text-slate-300 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl md: mr-5">
+            <p className="ml-5 mt-2 w-7/8 text-amber-50 lg:text-3xl text-justify md:leading-9 font-normal text-slate-300 drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-shadow-xl md: mr-5">
               Step into the forge with Yeeti, a master craftsman whose creations will empower you on your journey.            </p>
           </div>
         </div>
